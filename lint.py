c0c623591470666ad8697c06c4180d9642b3bd09
rbrook <cs123k@gmail.com>
2018-09-01 19:47:22 +0200

    configuring lint checker

---
+++ lint.py
@@ -0,0 +1,13 @@
+import glob
+import sys
+
+from pylint.lint import Run
+
+
+all_python_files = glob.glob('*.py', recursive=True)
+results = Run(all_python_files, do_exit=False)
+
+score = round(results.linter.stats['global_note'], 2)
+
+if score < 8:
+    raise Exception("Your code lint check scored only {}! You probably want to check Lint's messages and adjust your code.".format(score))
