0b3e418c0ea8b069250fda2c4c1d852cdff910c8
rbrook <cs123k@gmail.com>
2018-09-05 19:41:46 +0200

    adjusting run.sh to run only on CodeOcean

--- run.sh
+++ run.sh
@@ -3,9 +3,17 @@
 # This file is an execution file for CodeOcean cloud platform
 # It allows you to run the code without diving into the cumbersome process of installing and managing a
 # virtual environment, pipenv dependencies etc.
-# You can view a live example at https://codeocean.com/capsule/3477951b-31c5-4e45-8894-99e256fc5836/code
+# You can view a live example at https://codeocean.com/capsule/4705dc64-1815-48ec-882c-a803aea53908/code
 
 set -e  # stop on error
+
+# verify we're running on Code Ocean
+if [ `printenv PWD` != "/code" ]
+then
+        echo "This script is meant to run only on Code-Ocean platform."
+        echo "Please check https://codeocean.com/capsule/4705dc64-1815-48ec-882c-a803aea53908/code."
+        exit
+fi
 
 echo "installing pipenv.."
 pip3 install pipenv
@@ -20,4 +28,3 @@ echo "running APPLE picker.."
 pipenv run python3 apple.py  -s 78  -o ../results/  ../../input
 
 echo "Done. You can check the results in the right column."
-
